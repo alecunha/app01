cca8490881daedd66261150a4d3c3e69d64ec74a
Alessandro Cunha <alessandrocunha1@gmail.com>
2017-05-01 20:07:53 -0300

    Removendo imports não utilizados.

--- src/test/java/br/com/casadocodigo/teste/sistema/LoginPage.java
+++ src/test/java/br/com/casadocodigo/teste/sistema/LoginPage.java
@@ -3,9 +3,7 @@ package br.com.casadocodigo.teste.sistema;
 import static org.junit.Assert.*;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
